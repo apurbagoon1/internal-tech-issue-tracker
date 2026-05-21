fb578e978ecdd533a04382fe7080cc3c71f09271
Apurba Goon <apurbagoon1@gmail.com>
2026-05-21 17:26:36 +0600

    added postgres database connection and environment config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import dotenv from "dotenv";
-import app from "./app";
 
 dotenv.config();
 
+import app from "./app";
+import pool from "./config/db";
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await pool.query("SELECT NOW()");
+
+    console.log("Database Connected Successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
+  } 
+  catch (error) {
+    console.log("Failed to connect database", error);
+  }
+};
+
+startServer();
