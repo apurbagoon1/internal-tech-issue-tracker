94a12cf7c44b96f07aa0594acf48818a8dc9d969
Apurba Goon <apurbagoon1@gmail.com>
2026-05-23 16:57:58 +0600

    fix some issues for deployment

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,3 @@
 import app from "./app";
-import { initDB, pool } from "./db";
-
-const connectDB = async () => {
-  try {
-    await pool.query("SELECT NOW()");
-    await initDB();
-
-    console.log("Database Connected Successfully");
-  } 
-  catch (error) {
-    console.log("Database connection failed", error);
-  }
-};
-
-connectDB();
 
 export default app;
